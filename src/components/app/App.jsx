07385ac9f3aa024a7cffed54f8b60dd5214824c0
Nurjon <nurjon2477@gmail.com>
2023-12-03 16:56:34 +0500

    react-router-dom

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import { Box, } from "@mui/material"
+import {Routes , Route} from "react-router-dom"
+
+const App = () => {
+  return (
+    <Box >
+      <Routes >
+        <Route />
+      </Routes>
+    </Box>
+  )
+}
+
+export default App
